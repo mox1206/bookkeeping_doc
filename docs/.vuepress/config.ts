d1b6d0fa5d77b3cc1b3d1c213f6c73e58a265aa6
liujian890508@gmail.com <liujian890508@gmail.com>
2023-06-21 17:08:04 +0800

    upgrade vuepress version

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,46 @@
+/*
+ * @Author: moxiang
+ * @Date: 2023-01-11 16:58:58
+ * @LastEditors: moxiang
+ */
+import { defineUserConfig, defaultTheme } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: "微微记账",
+  dest: "./public",
+  description: "微微记账应用文档",
+  theme: defaultTheme({
+    logo: "/assets/img/icon.png",
+    contributors: false,
+    sidebar: [
+      "/",
+      "/problem",
+      "/update",
+      "/home",
+      {
+        text: "记账界面",
+        link: "/record",
+        collapsible: true,
+        children: [
+          "/record",
+          "/record/transfer",
+          "/record/reimburse",
+          "/record/settings",
+        ]
+      },
+      "/type_mgr",
+      {
+        text: "资产管理",
+        link: "/assets",
+        collapsible: true,
+        children: [
+          "/assets"
+        ]
+      },
+      "/mult_book",
+      "/user_agreement",
+      "/privacy_policy"
+    ]
+  })
+})
